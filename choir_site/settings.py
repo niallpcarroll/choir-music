97478d00e5ec4546ed8717716a2e3e2e3ac11559
Niall Carroll <155396080+niallpcarroll@users.noreply.github.com>
2025-10-08 17:36:41 +0000

    Create new folders for recordings and sheet_music

--- choir_site/settings.py
+++ choir_site/settings.py
@@ -32,7 +32,7 @@ if DEBUG:
     CSRF_TRUSTED_ORIGINS = [
         'http://localhost:8000',
         'http://127.0.0.1:8000',
-        f"https://{os.environ.get('CODESPACE_NAME', '')}-8000.app.github.dev"
+        f"https://{os.environ.get('CODESPACE_NAME', '')}-8000.app.github.dev",
     ]
 else:
     ALLOWED_HOSTS = ['your-production-domain.com']
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
+    # "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
